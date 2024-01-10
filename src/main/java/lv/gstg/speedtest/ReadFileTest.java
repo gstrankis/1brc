f276b198cb170bc8451606e05a77f12e11b264bf
Gatis Strankis <gatis.strankis@28stone.com>
2024-01-10 19:37:24 +0200

    misc file reading experiments

--- src/main/java/lv/gstg/speedtest/ReadFileTest.java
+++ src/main/java/lv/gstg/speedtest/ReadFileTest.java
@@ -7,29 +7,50 @@ import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ReadFileTest {
 
     private static final String FILE = "./measurements.txt";
+    private int bufferReadSize = 1;
 
     public static void main(String[] args) throws Exception {
-        new ReadFileTest().run();
+        System.out.println("availableProcessors=" + Runtime.getRuntime().availableProcessors());
+
+        System.out.println("--readBuffer--");
+        //readBufferTest();
+
+        System.out.println("--readChunksTest--");
+        //readChunksTest(8);
+
+        //System.exit(1);
+        System.out.println("--readFile--");
+        //new ReadFileTest(1).run();
+        //new ReadFileTest(64).run();
+        new ReadFileTest(1024).run();
+        //new ReadFileTest(1024 * 1024).run();
+    }
+
+    ReadFileTest(int bufferReadSize) {
+        this.bufferReadSize = bufferReadSize;
     }
 
     private void run() throws Exception {
-        readBufferTest();
-        // System.exit(1);
-        System.out.println("--readFile--");
 
-        // measure("mappedByteBufferParallel 1Th ", () -> mappedByteBufferParallel(1));
-        // measure("mappedByteBufferParallel 2Th", () -> mappedByteBufferParallel(2));
+        //measure("mappedByteBufferParallel  1Th", () -> mappedByteBufferParallel(1));
+        //measure("mappedByteBufferParallel  2Th", () -> mappedByteBufferParallel(2));
         measure("mappedByteBufferParallel  8Th", () -> mappedByteBufferParallel(8));
-        // measure("mappedByteBufferParallel 16Th", () -> mappedByteBufferParallel(16));
+        measure("mappedByteBufferParallel 16Th", () -> mappedByteBufferParallel(16));
         // measure("mappedByteBuffer 1Mb", () -> mappedByteBuffer(1024 * 1024));
-        measure("mappedByteBuffer 10Mb", () -> mappedByteBuffer(10 * 1024 * 1024));
-        // measure("mappedByteBuffer 1Gb", () -> mappedByteBuffer(1024 * 1024 * 1024));
-        measure("wrappedByteBuffer 1Mb", () -> wrappedByteBuffer(1024 * 1024));
+        measure("mappedByteBuffer    10Mb", () -> mappedByteBuffer(10 * 1024 * 1024));
+        // measure("mappedByteBuffer     1Gb", () -> mappedByteBuffer(1024 * 1024 * 1024));
+        // measure("wrappedByteBuffer  1Mb", () -> wrappedByteBuffer(1024 * 1024));
+        measure("wrappedByteBuffer   10Mb", () -> wrappedByteBuffer(10 * 1024 * 1024));
         // measure("bufferedInputStream 1Mb", () -> bufferedInputStream(1024 * 1024));
         measure("bufferedInputStream 10Mb", () -> bufferedInputStream(10 * 1024 * 1024));
         // measure("bufferedInputStream 100Mb", () -> bufferedInputStream(100 * 1024 * 1024));
@@ -39,27 +60,69 @@ public class ReadFileTest {
         // measure("bufferedReader 1Gb", () -> bufferedReader(1024 * 1024 * 1024));
     }
 
-    private void readBufferTest() throws Exception {
+    private static void readBufferTest() throws Exception {
         byte[] bytes = new byte[1024 * 1024 * 1024]; // 1Gb
+        var sizes = List.of(1, 64, 1024, 1024 * 1024);
+
         for (int i = 0; i < 3; i++) {
-            measure("readAllBytesOneByOne", () -> readAllBytesOneByOne(ByteBuffer.wrap(bytes)));
-            measure("readAllBytesUsingBuffer 1kb", () -> readAllBytesUsingBuffer(ByteBuffer.wrap(bytes), 1024));
-            measure("readAllBytesUsingBuffer 1Mb", () -> readAllBytesUsingBuffer(ByteBuffer.wrap(bytes), 1024 * 1024));
+            sizes.forEach(size -> {
+                var r = new ReadFileTest(size);
+                r.measure("readHeapBuffer", () -> r.readAllBytes(ByteBuffer.wrap(bytes)));
+            });
         }
     }
 
-    private long bufferedInputStream(final int bufferSize) {
+    private static void readChunksTest(int numberOfChunks) {
+        long started = System.currentTimeMillis();
+
+        var r = new ReadFileTest(1);
+        long bytes = 0;
+        var chunks = r.getChunks(numberOfChunks);
+        for (var chunk : chunks) {
+            System.out.println(chunk);
+            bytes += chunk.length;
+        }
+
+        System.out.printf("%-30s: %6dms bytes:%d%n", "readChunksTest",
+                (System.currentTimeMillis() - started), bytes);
+    }
+
+    private List<Chunk> getChunks(int numberOfChunks) {
+        var result = new ArrayList<Chunk>();
+        try (FileChannel ch = FileChannel.open(Paths.get(FILE), StandardOpenOption.READ)) {
+            long chunkSize = Math.min(ch.size() / numberOfChunks + 1, Integer.MAX_VALUE);
+            long offset = 0;
+//            byte[] buf = new byte[64];
+            while (offset < ch.size()) {
+                long length = Math.min(offset + chunkSize, ch.size()) - offset;
+                if (offset + length < ch.size()) {
+                    ch.position(offset + length);
+                    var bb = ByteBuffer.allocate(64);
+                    ch.read(bb);
+                    bb.rewind();
+                    while (bb.hasRemaining() && bb.get() != '\n')
+                        length++;
+                    //System.out.printf("adjust offset %-8d %s%n", offset, new String(buf).replace('\n', '$'));
+                }
+                result.add(new Chunk(offset, length));
+                offset += length;
+            }
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ReadResult bufferedInputStream(final int bufferSize) {
         try (InputStream in = new BufferedInputStream(Files.newInputStream(Paths.get(FILE)), bufferSize)) {
-            int b;
+            int bytesRead;
             byte[] buf = new byte[bufferSize];
-            long bytes = 0;
-            while ((b = in.read(buf)) >= 0) {
-                bytes += readAllBytes(ByteBuffer.wrap(buf));
-                // bytes += b;
+            ReadResult bytes = new ReadResult(0, 0);
+            while ((bytesRead = in.read(buf)) >= 0) {
+                bytes = bytes.add(readAllBytes(ByteBuffer.wrap(buf, 0, bytesRead)));
             }
             return bytes;
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -73,8 +136,7 @@ public class ReadFileTest {
                 bytes += b;
             }
             return bytes;
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -87,112 +149,120 @@ public class ReadFileTest {
                 bytes += line.length();
             }
             return bytes;
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private long mappedByteBuffer(final int bufferSize) {
+    private ReadResult mappedByteBuffer(final int bufferSize) {
         // https://www.geeksforgeeks.org/what-is-memory-mapped-file-in-java/
         // try (FileChannel ch = new RandomAccessFile(FILE, "r").getChannel()) {
         try (FileChannel ch = FileChannel.open(Paths.get(FILE), StandardOpenOption.READ)) {
             long offset = 0;
             byte[] buf = new byte[bufferSize];
-            long bytes = 0;
+            ReadResult result = new ReadResult(0, 0);
             while (offset < ch.size()) {
                 MappedByteBuffer bb = ch.map(FileChannel.MapMode.READ_ONLY, offset, Math.min(bufferSize, ch.size() - offset));
                 offset += bufferSize;
 
-                bytes += readAllBytes(bb);
+                result = result.add(readAllBytes(bb));
 
                 // bb.get(buf, 0, (int) length);
                 // bytes += length;
             }
-            return bytes;
-        }
-        catch (IOException e) {
+            return result;
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private long mappedByteBufferParallel(int nThreads) throws IOException, ExecutionException, InterruptedException {
+    private ReadResult mappedByteBufferParallel(int nThreads) throws IOException, ExecutionException, InterruptedException {
         var fileSize = new File(FILE).length();
-        var maxSegLength = fileSize / nThreads + 1;
+        var maxSegLength = Math.min(fileSize / nThreads + 1, Integer.MAX_VALUE);
         long offset = 0;
         var pool = Executors.newFixedThreadPool(nThreads);
-        Future<Long>[] futures = new Future[nThreads];
+        List<Future<ReadResult>> futures = new ArrayList<>();
         FileChannel ch = FileChannel.open(Paths.get(FILE), StandardOpenOption.READ);
-        for (int i = 0; i < nThreads; i++) {
+        while (offset < fileSize) {
             int segLength = (int) Math.min(fileSize - offset, maxSegLength);
             long finalOffset = offset;
-            futures[i] = pool.submit(() -> {
+            futures.add(pool.submit(() -> {
                 return mapAndReadAllBytes(ch, finalOffset, segLength);
-            });
+            }));
             offset += maxSegLength;
         }
 
-        long bytes = 0;
-        for (int i = 0; i < nThreads; i++) {
-            bytes += futures[i].get();
+        ReadResult result = new ReadResult(0, 0);
+        for (int i = 0; i < futures.size(); i++) {
+            result = result.add(futures.get(i).get());
         }
+        ch.close();
         pool.shutdown();
-        return bytes;
+        return result;
     }
 
-    private long mapAndReadAllBytes(FileChannel ch, long offset, int length) {
+    private ReadResult mapAndReadAllBytes(FileChannel ch, long offset, int length) {
         // https://www.geeksforgeeks.org/what-is-memory-mapped-file-in-java/
-        // try (FileChannel ch = new RandomAccessFile(FILE, "r").getChannel()) {
         try {
             MappedByteBuffer bb = ch.map(FileChannel.MapMode.READ_ONLY, offset, length);
             return readAllBytes(bb);
-
-            // byte[] buf = new byte[64 * 1024];
-            // //System.out.printf("mappedByteBuffer %12d %9d: capacity: %d limit: %d%n", offset, length, bb.capacity(), bb.limit());
-            // int bufOffset = 0;
-            // while (bufOffset < length) {
-            // bb.get(buf, 0, Math.min(buf.length, bb.remaining()));
-            // bufOffset += buf.length;
-            // }
-            // return bb.limit();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private long readAllBytes(ByteBuffer bb) {
-        return readAllBytesOneByOne(bb);
-        // return readAllBytesUsingBuffer(bb, 1024);
+    record Chunk(long offset, long length) {
     }
 
-    private long readAllBytesOneByOne(ByteBuffer bb) {
-        long bytes = 0;
+    private ReadResult readAllBytes(ByteBuffer bb) {
+        if (this.bufferReadSize == 1)
+            return readAllBytesOneByOne(bb);
+        else
+            return readAllBytesUsingBuffer(bb, this.bufferReadSize);
+    }
+
+    private ReadResult readAllBytesOneByOne(ByteBuffer bb) {
+        //while (bb.hasRemaining()) {
+//mappedByteBufferParallel  8Th  buf:1b  :   4350ms ReadResult[bytes=13795372587, lines=1000000000]
+//mappedByteBuffer    10Mb       buf:1b  :  14064ms ReadResult[bytes=13795372587, lines=1000000000]
+//wrappedByteBuffer   10Mb       buf:1b  :  16648ms ReadResult[bytes=13795372587, lines=1000000000]
+//bufferedInputStream 10Mb       buf:1b  :  19147ms ReadResult[bytes=13795372587, lines=1000000000]
+//for (int i = 0; i < limit; i++) {
+//mappedByteBufferParallel  8Th  buf:1b  :   5862ms ReadResult[bytes=13795372587, lines=1000000000]
+//mappedByteBuffer    10Mb       buf:1b  :  15270ms ReadResult[bytes=13795372587, lines=1000000000]
+//wrappedByteBuffer   10Mb       buf:1b  :  15047ms ReadResult[bytes=13795372587, lines=1000000000]
+//bufferedInputStream 10Mb       buf:1b  :  16711ms ReadResult[bytes=13795372587, lines=1000000000]
+
         long lines = 0;
         // read bytes one-by-one
         while (bb.hasRemaining()) {
-            var b = bb.get();
-            bytes++;
-            if (b == '\n')
-                lines += consumeByte(b);
+            //final var limit = bb.limit();
+            //for (int i = 0; i < limit; i++) { //no big difference/worse
+            final var b = bb.get();
+            lines += consumeByte(b);
+//            if (b == '\n')
+//                lines++;
         }
-        return bytes + lines;
+        return new ReadResult(bb.limit(), lines);
     }
 
-    private long readAllBytesUsingBuffer(ByteBuffer bb, int bufferSize) {
+    private ReadResult readAllBytesUsingBuffer(ByteBuffer bb, int bufferSize) {
         long bytes = 0;
         long lines = 0;
 
         // read by copying to buf array
         byte[] buf = new byte[bufferSize];
         while (bb.hasRemaining()) {
-            var len = Math.min(buf.length, bb.remaining());
-            bb.get(buf, 0, len);
-            bytes += len;
-            for (int i = 0; i < buf.length; i++)
-                lines += consumeByte(buf[i]);
+            var length = Math.min(buf.length, bb.remaining());
+            bb.get(buf, 0, length);
+            bytes += length;
+            for (int i = 0; i < length; i++) {
+                //lines += consumeByte(buf[i]);
+                if (buf[i] == '\n')
+                    lines++;
+            }
         }
-        return bytes + lines;
+        return new ReadResult(bytes, lines);
     }
 
     private int consumeByte(byte b) {
@@ -203,27 +273,73 @@ public class ReadFileTest {
             return 0;
     }
 
-    private long wrappedByteBuffer(final int bufferSize) {
+    private ReadResult wrappedByteBuffer(final int bufferSize) {
+//reusing byteBuffer
+//wrappedByteBuffer   10Mb       buf:1kb :  11229ms ReadResult[bytes=13795372587, lines=1000000000]
+//wrappedByteBuffer   10Mb       buf:1kb :  10392ms ReadResult[bytes=13795372587, lines=1000000000]
+//new ByteBuffer for each read
+//wrappedByteBuffer   10Mb       buf:1kb :  10302ms ReadResult[bytes=13795372587, lines=1000000000]
         try (FileChannel ch = FileChannel.open(Paths.get(FILE), StandardOpenOption.READ)) {
             byte[] buf = new byte[bufferSize];
-            ByteBuffer bb = ByteBuffer.wrap(buf);
+            ByteBuffer bb; //= ByteBuffer.wrap(buf);
             int read;
-            long bytes = 0;
-            while ((read = ch.read(bb)) >= 0) {
+            ReadResult result = new ReadResult(0, 0);
+            while ((read = ch.read(bb = ByteBuffer.wrap(buf))) >= 0) {
                 bb.rewind();
-                bytes += readAllBytes(bb);
-                bb.rewind();
+                bb.limit(read);
+                result = result.add(readAllBytes(bb));
+                //bb.rewind();
             }
-            return bytes;
-        }
-        catch (IOException e) {
+            return result;
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void measure(String name, Callable<Long> r) throws Exception {
-        long started = System.currentTimeMillis();
-        long bytes = r.call();
-        System.out.printf("%-30s: %6dms bytes:%d%n", name, (System.currentTimeMillis() - started), bytes);
+    private void measure(String name, Callable<ReadResult> r) {
+        try {
+            long started = System.currentTimeMillis();
+            ReadResult result = r.call();
+            System.out.printf("%-30s buf:%-4s: %6dms %s%n", name,
+                    formatBytes(this.bufferReadSize),
+                    (System.currentTimeMillis() - started),
+                    result);
+            System.gc();
+            Thread.sleep(100);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private String formatBytes(int bytes) {
+        if (bytes >= 1073741824)
+            return (bytes / 1073741824) + "Gb";
+        if (bytes >= 1048576)
+            return (bytes / 1048576) + "Mb";
+        if (bytes >= 1024)
+            return (bytes / 1024) + "kb";
+        return bytes + "b ";
+    }
+
+    private static record ReadResult(long bytes, long lines) {
+        ReadResult add(ReadResult that) {
+            return new ReadResult(this.bytes + that.bytes, this.lines + that.lines);
+        }
+    }
+
+    private static class Nanos {
+        List<String> prints = new ArrayList<>();
+        long started = System.currentTimeMillis();
+
+        void snap(String name) {
+            long done = System.currentTimeMillis();
+            prints.add(String.format("%6dms for %s", (done - started), name));
+            started = System.currentTimeMillis();
+        }
+
+        void printout() {
+            prints.forEach(System.out::println);
+        }
+    }
+
 }
